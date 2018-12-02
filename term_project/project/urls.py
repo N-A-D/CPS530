bbb65dc39eedd1e8d8d314b7bc89e582e74e1880
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-01 23:57:26 -0500

    Added tutorial page

--- term_project/project/urls.py
+++ term_project/project/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.summary, name='summary'),
 	path('installations/', views.installations, name='installations'),
-    # path('tutorial/', views.tutorial, name='tutorial'),	
+    path('tutorial/', views.tutorial, name='tutorial'),	
     # path('webpage/', views.webpage, name='webpage'),	
 	path('report/', views.report, name='report'),
 	path('credits/', views.credits, name='credits'),
