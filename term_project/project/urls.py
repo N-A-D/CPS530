1d73f1a4ff369719526049c81e8cdf0b5447719e
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-02 00:12:00 -0500

    Added custom page

--- term_project/project/urls.py
+++ term_project/project/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 	path('', views.summary, name='summary'),
 	path('installations/', views.installations, name='installations'),
     path('tutorial/', views.tutorial, name='tutorial'),	
-    # path('webpage/', views.webpage, name='webpage'),	
+    path('webpage/', views.webpage, name='webpage'),	
 	path('report/', views.report, name='report'),
 	path('credits/', views.credits, name='credits'),
 ]
